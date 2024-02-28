459d79742e4593e07d8ed359d1bca811ceb84d11
brunomarcel458 <109152699+brunomarcel458@users.noreply.github.com>
2024-02-28 13:25:08 -0300

    created routes to get the css

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="<?=$base;?>/assets/css/style.css">
+    <title>Meu CRUD</title>
 </head>
 <body>
     <h1>Título do Site</h1>
